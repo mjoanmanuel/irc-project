ec16c70e6527da227396510b58f5efdc85bf49df
jmendoza <mjoanmanuel@gmail.com>
2011-11-09 01:14:30 -0400

    Adding new module.

---
+++ irc-server/src/main/java/com/project/ircserver/IRCServer.java
@@ -0,0 +1,18 @@
+package com.project.ircserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * IRCServer is responsible of
+ * 
+ * @author mjoanmanuel@gmail.com
+ */
+// TODO implement
+public class IRCServer extends ServerSocket {
+
+    public IRCServer() throws IOException {
+	super();
+    }
+
+}
