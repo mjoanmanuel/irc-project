9444289d344c052bd3c565358f26241633ce86da
jmendoza <mjoanmanuel@gmail.com>
2011-11-10 00:57:26 -0400

    Working on IRCServer

--- irc-server/src/main/java/com/project/ircserver/IRCServer.java
+++ irc-server/src/main/java/com/project/ircserver/IRCServer.java
@@ -1,18 +1,42 @@
 package com.project.ircserver;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.net.ServerSocket;
 
 /**
  * IRCServer is responsible of
  * 
  * @author mjoanmanuel@gmail.com
+ * 
+ *         TODO add timeout
  */
-// TODO implement
-public class IRCServer extends ServerSocket {
+public class IRCServer extends ServerSocket implements Serializable {
 
-    public IRCServer() throws IOException {
-	super();
+    private static final long serialVersionUID = 1L;
+
+    private String serverName;
+    private String host; // TODO implement correctly
+    private int port;
+
+    public IRCServer(final String serverName, final String host, final int port)
+	    throws IOException {
+	super(port);
+	this.serverName = serverName;
+	this.host = host;
+	this.port = port;
+    }
+
+    public String getServerName() {
+	return serverName;
+    }
+
+    public String getHost() {
+	return host;
+    }
+
+    public int getPort() {
+	return port;
     }
 
 }
