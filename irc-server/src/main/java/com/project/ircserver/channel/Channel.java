9b8f6b36eb2883ec2047ea473c0147544d9d7b49
jmendoza <mjoanmanuel@gmail.com>
2012-01-03 01:03:16 -0400

    Working on irc channel.

--- irc-server/src/main/java/com/project/ircserver/channel/Channel.java
+++ irc-server/src/main/java/com/project/ircserver/channel/Channel.java
@@ -4,50 +4,101 @@ import static java.lang.String.format;
 
 import java.io.PrintStream;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.ircclient.Client;
 
 /**
- * IRCChannel is responsible of
+ * Channel is responsible of an IRC channel.
+ * 
+ * A channel is a named group of one or more clients which will all receive
+ * messages addressed to that channel. The channel is created implicitly when
+ * the first client joins it, and the channel ceases to exist when the last
+ * client leaves it. While channel exists, any client can reference the channel
+ * using the name of the channel.
+ * 
+ * Channels names are strings (beginning with a '&' or '#' character) of length
+ * up to 200 characters. Apart from the the requirement that the first character
+ * being either '&' or '#'; the only restriction on a channel name is that it
+ * may not contain any spaces (' '), a control G (^G or ASCII 7), or a comma
+ * (',' which is used as a list item separator by the protocol).
+ * 
+ * There are two types of channels allowed by this protocol. One is a
+ * distributed channel which is known to all the servers that are connected to
+ * the network. These channels are marked by the first character being a only
+ * clients on the server where it exists may join it. These are distinguished by
+ * a leading '&' character. On top of these two types, there are the various
+ * channel modes available to alter the characteristics of individual channels.
+ * See section 4.2.3 (MODE command) for more details on this.
+ * 
+ * To create a new channel or become part of an existing channel, a user is
+ * required to JOIN the channel. If the channel doesn't exist prior to joining,
+ * the channel is created and the creating user becomes a channel operator. If
+ * the channel already exists, whether or not your request to JOIN that channel
+ * is honoured depends on the current modes of the channel. For example, if the
+ * channel is invite-only, (+i), then you may only join if invited. As part of
+ * the protocol, a user may be a part of several channels at once, but a limit
+ * of ten (10) channels is recommended as being ample for both experienced and
+ * novice users. See section 8.13 for more information on this.
+ * 
+ * If the IRC network becomes disjoint because of a split between two servers,
+ * the channel on each side is only composed of those clients which are
+ * connected to servers on the respective sides of the split, possibly ceasing
+ * to exist on one side of the split. When the split is healed, the connecting
+ * servers announce to each other who they think is in each channel and the mode
+ * of that channel. If the channel exists on both sides, the JOINs and MODEs are
+ * interpreted in an inclusive manner so that both sides of the new connection
+ * will agree about which clients are in the channel and what modes the channel
+ * has.
  * 
  * @author mjoanmanuel@gmail.com
  */
 public class Channel implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String channelName;
-	private PrintStream writeToClient;
+    private String channelName;
+    // This property holds a channel with registered clients.
+    private Map<String, HashMap<String, Client>> channel = new HashMap<String, HashMap<String, Client>>();
+    private PrintStream writeToClient;
 
-	public Channel(final String channelName, final PrintStream writeToClient) {
-		this.channelName = channelName;
-		this.writeToClient = writeToClient;
-		sendJoinMsg();
-	}
+    public Channel(final String channelName, final PrintStream writeToClient) {
+	this.channelName = channelName;
+	this.writeToClient = writeToClient;
+	sendJoinMsg();
+    }
 
-	public Channel setChannel(final String channelName) {
-		this.channelName = channelName;
-		return this;
-	}
+    public Channel setChannel(final String channelName) {
+	this.channelName = channelName;
+	return this;
+    }
 
-	public String getChannel() {
-		return channelName;
-	}
+    public String getChannel() {
+	return channelName;
+    }
 
-	public Channel setIrcClient(final PrintStream writeToClient) {
-		this.writeToClient = writeToClient;
-		return this;
-	}
+    public Channel setIrcClient(final PrintStream writeToClient) {
+	this.writeToClient = writeToClient;
+	return this;
+    }
 
-	public PrintStream getIrcClient() {
-		return writeToClient;
-	}
+    public PrintStream getIrcClient() {
+	return writeToClient;
+    }
 
-	public void sendMsg(final String msg) {
-		writeToClient.println(msg);
-	}
+    public void sendMsg(final String msg) {
+	writeToClient.println(msg);
+    }
 
-	private void sendJoinMsg() {
-		// send join msg to connected client.
-		writeToClient.println(format("JOIN # '%s' ", channelName));
-	}
+    /** @return a Map containing all the clients in the specify channel. */
+    public HashMap<String, Client> getRegisteredClients(final String channelName) {
+	return channel.get(channelName);
+    }
+
+    private void sendJoinMsg() {
+	// send join msg to connected client.
+	writeToClient.println(format("JOIN # '%s' ", channelName));
+    }
 
 }
