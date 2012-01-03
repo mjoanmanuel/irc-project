c1de8a3463dc5bd5cfe702889a2c036a0add553f
jmendoza <mjoanmanuel@gmail.com>
2012-01-03 01:02:51 -0400

    Working with IRC server and protocol

--- irc-server/src/main/java/com/project/ircserver/Server.java
+++ irc-server/src/main/java/com/project/ircserver/Server.java
@@ -3,19 +3,69 @@
  */
 package com.project.ircserver;
 
+import static java.lang.System.out;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import com.project.ircclient.Client;
+import com.project.ircserver.channel.Channel;
+import com.project.ircserver.protocol.Protocol;
+
 /**
+ * Server is an local IRC server.
+ * 
  * @author jmendoza Dec 1, 2011
  */
 public class Server extends ServerSocket {
 
-	/** Max client allowed per channel. */
-	public static final int MAX_CLIENTS_ALLOWED = 30;
-	
-	public Server(int port) throws IOException {
-		super(port);
-	}
+    /** Max client allowed per channel. */
+    public static final int MAX_CLIENTS_ALLOWED = 30;
+    private final boolean listening = true;
+    /** All servers must have the following information about all clients. */
+    private final Protocol protocol = new Protocol();
+    private int port;
+    private Channel channel;
 
+    public Server(final int port) throws IOException {
+	super(port);
+	this.port = port;
+    }
+
+    /** Connect and register this nickname to the server. */
+    public boolean connect(final String channelName, final String nickname) {
+	while (listening) {
+	    if (protocol.isChannelRegistered(channelName)) {
+		// If the channel doesn't exist, is created implicitly.
+		protocol.registerChannel(channelName, null);
+	    }
+	    if (protocol.isNickNameRegistered(nickname)) {
+		connectNewClient(nickname);
+		return true;
+	    }
+
+	    // TODO : reconnect with other nickname.
+	    return false;
+	}
+    }
+
+    public Client findClientByNickname(final String nickname) {
+	return protocol.findClientByNickname(nickname);
+    }
+
+    private void connectNewClient(final String nickname) {
+	Client client;
+	try {
+	    // This is part of RFC 1.2
+	    client = (Client) this.accept(); // we get the socket.
+	    client.setNickname(nickname); // set the nickname.
+	    client.setServer(this); // set the server where the client is
+				    // connected.
+	    protocol.registerClient(nickname, client);
+
+	    new Worker(client, protocol).start();
+	} catch (IOException e) {
+	    out.println(" Couldn't connect to port " + port + " try other one.");
+	}
+    }
 }
--- irc-server/src/main/java/com/project/ircserver/protocol/Protocol.java
+++ irc-server/src/main/java/com/project/ircserver/protocol/Protocol.java
@@ -1,10 +1,91 @@
 package com.project.ircserver.protocol;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.ircclient.Client;
+import com.project.ircserver.channel.Channel;
+
 /**
- * IRCProtocol is responsible of
+ * Protocol is responsible of handling messages.
  * 
  * @author mjoanmanuel@gmail.com
  */
-public class IRCProtocol {
+public class Protocol {
+
+    /**
+     * Represents an regular expression for validating nicknames from a-z | A-Z
+     * containing 0-9 and must start with a character as minimun and must has 9
+     * as maximum.
+     */
+    private static final String NICKNAME_REGEX = "[a-z0-9A-Z0-9]{1,9}";
+
+    /** Must match the patter #|& channelName1298734 without space */
+    private static final String CHANNELNAME_REGEX = "^#|&[a-zA-z]\\S{1,200}";
+
+    /**
+     * Holds the clients in the server making them unique.
+     * 
+     * @param String
+     *            - is the nickname.
+     * 
+     * @param Client
+     *            - hold the client Entity.
+     */
+    private final Map<String, Client> clients = new HashMap<String, Client>(0);
+
+    /**
+     * Holds the channels in the server making them unique.
+     * 
+     * @param String
+     *            is the channel name
+     * @param Channel
+     *            hold the channel Entity containing a list of clients.
+     */
+    private final Map<String, Channel> channels = new HashMap<String, Channel>(
+	    0);
+
+    public Protocol() {
+    }
+
+    /** IRC Protocol said that proper nickname is lenght of 9. */
+    public boolean validateNickname(final String nickname) {
+	return nickname.matches(NICKNAME_REGEX);
+    }
+
+    public boolean validateChannelName(final String channelName) {
+	return channelName.matches(CHANNELNAME_REGEX);
+    }
+
+    public Client findClientByNickname(final String nickname) {
+	return clients.get(nickname);
+    }
+
+    /** Puts a new client into the clients Map. */
+    public void registerClient(final String nickname, final Client client) {
+	clients.put(nickname, client);
+    }
+
+    public void registerChannel(final String channelName, final Channel channel) {
+	channels.put(channelName, channel);
+    }
+
+    /** Updates the clients Map. */
+    public void updateClients(final HashMap<String, Client> newClients) {
+	newClients.putAll(newClients);
+    }
+
+    /** Validates if the nickname passed is already registered */
+    public boolean isNickNameRegistered(final String nickname) {
+	return clients.containsKey(nickname);
+    }
+
+    public boolean isChannelRegistered(final String channel) {
+	return channels.containsKey(channel);
+    }
+
+    public void handleMessage(final String message) {
+	// TODO
+    }
 
 }
