4697f63b4d5ad30a64f4f9dbad3696feeb856223
jmendoza <mjoanmanuel@gmail.com>
2012-01-03 01:03:05 -0400

    Updating changes.

--- irc-server/src/main/java/com/project/ircserver/Connector.java
+++ irc-server/src/main/java/com/project/ircserver/Connector.java
@@ -11,7 +11,7 @@ import java.net.UnknownHostException;
 import com.project.ircserver.channel.Channel;
 
 /**
- * IRCServer is responsible of
+ * Connector is responsible of handle an external connection.
  * 
  * @author mjoanmanuel@gmail.com
  * 
@@ -19,54 +19,59 @@ import com.project.ircserver.channel.Channel;
  */
 public class Connector implements Serializable {
 
-	/** Default IRC port defined by RFC document. */
-	public static final int DEFAULT_IRC_PORT = 6667;
-	private static final long serialVersionUID = 1L;
-	private String host;
-	private int port;
-	private PrintStream writeTo = null;
+    /** Default IRC port defined by RFC document. */
+    public static final int DEFAULT_IRC_PORT = 6667;
+    private static final long serialVersionUID = 1L;
+    private String host;
+    private int port;
+    private PrintStream writeTo = null;
 
-	public Connector(final String host) throws UnknownHostException, IOException {
-		this(host, DEFAULT_IRC_PORT);
+    public Connector(final int port) throws UnknownHostException, IOException {
+	this.port = port;
+    }
+
+    public Connector(final String host) throws UnknownHostException,
+	    IOException {
+	this(host, DEFAULT_IRC_PORT);
+    }
+
+    public Connector(final String host, final int port)
+	    throws UnknownHostException, IOException {
+	this.host = host;
+	this.port = port;
+    }
+
+    public String getHost() {
+	return host;
+    }
+
+    public Channel getChannel(final String channelName) {
+	return new Channel(channelName, writeTo);
+    }
+
+    public int getPort() {
+	return port;
+    }
+
+    public Socket connect() {
+	Socket socket;
+	try {
+	    socket = new Socket(host, DEFAULT_IRC_PORT);
+	    writeTo = new PrintStream(socket.getOutputStream());
+	    return socket;
+	} catch (UnknownHostException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+	return null;
 
-	public Connector(final String host, final int port)
-			throws UnknownHostException, IOException {
-		this.host = host;
-		this.port = port;
-	}
+    }
 
-	public String getHost() {
-		return host;
-	}
-
-	public Channel getChannel(final String channelName) {
-		return new Channel(channelName, writeTo);
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public Socket connect() {
-		Socket socket;
-		try {
-			socket = new Socket(host, DEFAULT_IRC_PORT);
-			writeTo = new PrintStream(socket.getOutputStream());
-			return socket;
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-
-	}
-
-	public void register() {
-		final String nickname = "test";
-		final String hosted = "locahost";
-		writeTo.println(format("nickname: '%s', host: '%s'", nickname, hosted));
-	}
+    public void register() {
+	final String nickname = "test";
+	final String hosted = "locahost";
+	writeTo.println(format("nickname: '%s', host: '%s'", nickname, hosted));
+    }
 
 }
