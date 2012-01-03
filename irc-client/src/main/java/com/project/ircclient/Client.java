769f0ef23646a275c27c126c3fe9b662c04f89f0
jmendoza <mjoanmanuel@gmail.com>
2012-01-03 01:01:14 -0400

    Adding server to irc client entity.

--- irc-client/src/main/java/com/project/ircclient/Client.java
+++ irc-client/src/main/java/com/project/ircclient/Client.java
@@ -2,14 +2,56 @@ package com.project.ircclient;
 
 import java.net.Socket;
 
+import com.project.ircserver.Server;
+
 /**
- * IRCClient is responsible of
+ * Client is an IRCClient IRC
  * 
  * @author mjoanmanuel@gmail.com
  */
-public class IRCClient extends Socket {
+public class Client extends Socket {
 
-    public IRCClient() {
+    private static final String OPERATOR_IDENTIFIER = "@";
+    private String nickname; // holds the nickname for the client.
+    private Server server; // client connected to the irc server.
+    // Operator can do whatever he/she wants. TODO : see if we can implement
+    // this.
+    private boolean isOperator;
+
+    public Client() {
+    }
+
+    public Client setNickname(final String nickname) {
+	this.nickname = nickname;
+	if (hasOperatorIdentifier(nickname)) {
+	    isOperator = true;
+	}
+	return this;
+    }
+
+    public String getNickname() {
+	return nickname;
+    }
+
+    public String getRealHost() {
+	return getInetAddress().getHostName();
+    }
+
+    public Client setServer(final Server server) {
+	this.server = server;
+	return this;
+    }
+
+    public Server getServer() {
+	return server;
+    }
+
+    public boolean isOperator() {
+	return isOperator;
+    }
+
+    private boolean hasOperatorIdentifier(final String nickname) {
+	return nickname.contains(OPERATOR_IDENTIFIER);
     }
 
 }
